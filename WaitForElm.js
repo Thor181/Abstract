40da7a6cc8b09cf514c03d927bed9cb0b6f8a084
Artem <63106764+Thor181@users.noreply.github.com>
2023-11-19 15:26:41 +0300

    Create WaitForElm.js

---
+++ WaitForElm.js
@@ -0,0 +1,28 @@
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                observer.disconnect();
+                resolve(document.querySelector(selector));
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
+//To use
+waitForElm('.some-class').then((elm) => {
+    console.log('Element is ready');
+    console.log(elm.textContent);
+});
+
+//To use 2
+const elm = await waitForElm('.some-class');

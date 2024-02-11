484bcc3b299c8ee75038d0f3fa1a27b9aa75e3ee
Artem <63106764+Thor181@users.noreply.github.com>
2024-02-11 15:08:27 +0300

    Update Code.cs

--- Becnhmarks/Lazy/1. Simple using/Code.cs
+++ Becnhmarks/Lazy/1. Simple using/Code.cs
@@ -1,1 +1,35 @@
+[MemoryDiagnoser]
+public class Test
+{
+    [Benchmark]
+    public void LazyClass()
+    { 
+        var cl = new LazyClass();
+    }
 
+    [Benchmark]
+    public void PlainClass()
+    {
+        var cl = new PlainClass();
+    }
+}
+
+public class LazyClass
+{
+    public Lazy<Dictionary<string, string>> Dict { get; set; }
+
+    public LazyClass()
+    {
+        Dict = new Lazy<Dictionary<string, string>>(() => new Dictionary<string, string>() { { "key1", "value1" }, { "key2", "value2" } }) ;
+    }
+}
+
+public class PlainClass
+{
+    public Dictionary<string, string> Dict { get; set; }
+
+    public PlainClass()
+    {
+        Dict = new Dictionary<string, string>() { { "key1", "value1" }, { "key2", "value2" } };
+    }
+}

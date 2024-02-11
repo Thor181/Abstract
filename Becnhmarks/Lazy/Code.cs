f54c0edbe5e40fb75cf3b7dcf99eb421fb625b36
Artem <63106764+Thor181@users.noreply.github.com>
2024-02-11 14:51:34 +0300

    Lazy

---
+++ Becnhmarks/Lazy/Code.cs
@@ -0,0 +1,46 @@
+namespace Benchmarks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Test>();
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class Test
+    {
+        [Benchmark]
+        public void LazyClass()
+        { 
+            var cl = new LazyClass();
+        }
+
+        [Benchmark]
+        public void PlainClass()
+        {
+            var cl = new PlainClass();
+        }
+    }
+
+    public class LazyClass
+    {
+        public Lazy<Dictionary<string, string>> Dict { get; set; }
+
+        public LazyClass()
+        {
+            Dict = new Lazy<Dictionary<string, string>>(() => new Dictionary<string, string>() { { "key1", "value1" }, { "key2", "value2" } }) ;
+        }
+    }
+
+    public class PlainClass
+    {
+        public Dictionary<string, string> Dict { get; set; }
+
+        public PlainClass()
+        {
+            Dict = new Dictionary<string, string>() { { "key1", "value1" }, { "key2", "value2" } };
+        }
+    }
+}
